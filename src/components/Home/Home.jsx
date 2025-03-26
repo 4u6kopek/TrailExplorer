cb3a4429e61d37fb09ab5005409badb446582a82
Bogomil Dimitrov <foxyeku4e@gmail.com>
2025-03-26 22:27:13 +0200

    Test DB connection

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,15 +1,23 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "../../App.css";
 import Cards from "../Cards/Cards";
 import HeroSection from "../HeroSection/HeroSection";
 import Footer from "../Footer/Footer";
 
 function Home() {
+  const [trails, setTrails] = useState([]);
+
+  useEffect(() => {
+    fetch("https://trailexplorerbackend.vercel.app/api/trails")
+      .then((res) => res.json())
+      .then((data) => setTrails(data))
+      .catch((err) => console.error("Error fetching trails:", err));
+  }, []);
 
   return (
     <>
       <HeroSection />
-      <Cards />
+      <Cards trails={trails} />
       <Footer />
     </>
   );
