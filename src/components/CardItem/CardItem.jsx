2aa3d4954060be49956c534989217673ded2c451
Bogomil Dimitrov <foxyeku4e@gmail.com>
2025-03-29 15:26:38 +0200

    Temp cards active items

--- src/components/CardItem/CardItem.jsx
+++ src/components/CardItem/CardItem.jsx
@@ -1,24 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const CardItem = ({ path, label, src, text }) => (
-  <li className="cards__item">
-    <Link className="cards__item__link" to={path}>
-      <figure className="cards__item__pic-wrap" data-category={label}>
-        <img className="cards__item__img" alt="Travel" src={src} />
-      </figure>
-      <div className="cards__item__info">
-        <h5 className="cards__item__text">{text}</h5>
-      </div>
-    </Link>
-  </li>
-);
-
-CardItem.defaultProps = {
-  path: "/default-path",
-  label: "Default Label",
-  src: "/default-img.jpg",
-  text: "Default Text",
+const CardItem = ({ src, text, label, path }) => {
+  return (
+    <li className="card-item">
+      <Link className="card-item__link" to={path}>
+        <figure className="card-item__pic-wrap" data-category={label}>
+          <img src={src} alt="Trail" className="card-item__img" />
+        </figure>
+        <div className="card-item__info">
+          <h5 className="card-item__text">{text}</h5>
+        </div>
+      </Link>
+    </li>
+  );
 };
 
 export default CardItem;
--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -2,50 +2,31 @@ import React from "react";
 import "./Cards.css";
 import CardItem from "../CardItem/CardItem";
 
-function Cards() {
+const Cards = ({ trails }) => {
   return (
     <div className="cards">
       <h1>Check out these EPIC Destinations!</h1>
       <div className="cards__container">
         <div className="cards__wrapper">
           <ul className="cards__items">
-            <CardItem
-              src="images/img-9.jpg"
-              text="Explore the hidden waterfall deep inside the Amazon Jungle"
-              label="Adventure"
-              path="/services"
-            />
-            <CardItem
-              src="images/img-2.jpg"
-              text="Travel through the Islands of Bali in a Private Cruise"
-              label="Luxury"
-              path="/services"
-            />
-          </ul>
-          <ul className="cards__items">
-            <CardItem
-              src="images/img-3.jpg"
-              text="Set Sail in the Atlantic Ocean visiting Uncharted Waters"
-              label="Mystery"
-              path="/services"
-            />
-            <CardItem
-              src="images/img-4.jpg"
-              text="Experience Football on Top of the Himilayan Mountains"
-              label="Adventure"
-              path="/products"
-            />
-            <CardItem
-              src="images/img-8.jpg"
-              text="Ride through the Sahara Desert on a guided camel tour"
-              label="Adrenaline"
-              path="/sign-up"
-            />
+            {trails.length > 0 ? (
+              trails.map((trail) => (
+                <CardItem
+                  key={trail._id}
+                  src={trail.image || "/img-1.jpg"}
+                  text={trail.description}
+                  label={trail.category}
+                  path={`/trail/${trail._id}`} // Use MongoDB _id in URL
+                />
+              ))
+            ) : (
+              <p>No trails found.</p>
+            )}
           </ul>
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default Cards;
