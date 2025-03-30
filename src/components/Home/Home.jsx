cfa70a45018b4839877e93193d754e85d4256154
Bogomil Dimitrov <foxyeku4e@gmail.com>
2025-03-31 02:37:48 +0300

    Home page fetching data from api

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -6,13 +6,29 @@ import Footer from "../Footer/Footer";
 
 function Home() {
   const [trails, setTrails] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-    fetch("https://trailexplorerbackend.vercel.app/api/trails")
-      .then((res) => res.json())
-      .then((data) => setTrails(data))
-      .catch((err) => console.error("Error fetching trails:", err));
+    fetch(
+      "https://trail-explorer-backend-git-main-bogomils-projects-951e1882.vercel.app/api/trails"
+    )
+      .then((res) => {
+        if (!res.ok) throw new Error("Failed to fetch");
+        return res.json();
+      })
+      .then((data) => {
+        setTrails(data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        setError(err.message);
+        setLoading(false);
+      });
   }, []);
+
+  if (loading) return <div>Loading trails...</div>;
+  if (error) return <div>Error: {error}</div>;
 
   return (
     <>
